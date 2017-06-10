067fd9e6982d319ea51f58628890faea2b794ab6
CiarlantiniD <dario.ciarlantini@outlook.com>
2017-06-10 18:19:15 -0300

    Se mide la fuerza con la que colisiona

--- MoonLander/Assets/NaveScript.cs
+++ MoonLander/Assets/NaveScript.cs
@@ -6,15 +6,20 @@ public class NaveScript : MonoBehaviour {
 
     Rigidbody2D rb;
 
+	float timer = 0f;
+	bool gravityEstable = false;
+
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+		rb.gravityScale = 0.00001f;
+		rb.AddForce(Vector2.right * 20);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-
+		timer += Time.deltaTime;
 
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.eulerAngles = new Vector3 (0, 0, 15);
@@ -26,7 +31,9 @@ public class NaveScript : MonoBehaviour {
 			transform.eulerAngles = new Vector3 (0, 0, 0);
 		}
 
-
+		if (gravityEstable || timer > 5.0f){
+			rb.gravityScale = 0.2f;
+		}
 
 
 	}
@@ -36,6 +43,7 @@ public class NaveScript : MonoBehaviour {
 		if (Input.GetKey(KeyCode.Space))
 		{
 			rb.AddForce(Vector2.up * 5);
+			gravityEstable = true;
 
 			if (Input.GetKey (KeyCode.LeftArrow)) {
 				rb.AddForce (Vector2.left * 2);
@@ -45,4 +53,18 @@ public class NaveScript : MonoBehaviour {
 			}
 		}
     }
+
+
+	void OnCollisionEnter2D(Collision2D coll){
+
+		//print(rb.velocity.magnitude);
+
+		if (rb.velocity.magnitude > 0.0002f) {
+			print ("Perdiste");
+		} else {
+			print ("Ganaste");
+		}
+	}
+
+
 }
