ddde43f603930a2c625aba92c5ec6c8e8b8c622e
CiarlantiniD <dario.ciarlantini@outlook.com>
2017-06-24 17:23:04 -0300

    Mejoras mínimas en el manejo del menú

--- MoonLander/Assets/Scripts/MainMenu/MainMenu.cs
+++ MoonLander/Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,13 +5,11 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
-	Text[] textOptions; // = new Text[]{0,1,2,3};
 
-
-/*	Text continueGame;
+	Text continueGame;
 	Text newGame;
 	Text optionsGame;
-	Text exitGame;*/
+	Text exitGame;
 
 	int optionSlect = 0;
 	int maxOption = 3;
@@ -20,35 +18,45 @@ public class MainMenu : MonoBehaviour {
 	Color notSelectColor = Color.gray;
 
 	void Awake () {
-		/*continueGame=transform.GetChild(0).GetComponent<Text>();
+		continueGame=transform.GetChild(0).GetComponent<Text>();
 		newGame=transform.GetChild(1).GetComponent<Text>();
 		optionsGame=transform.GetChild(2).GetComponent<Text>();
-		exitGame=transform.GetChild(3).GetComponent<Text>();*/
+		exitGame=transform.GetChild(3).GetComponent<Text>();
 
-		Text[] textOptions = new Text[] {
-			transform.GetChild (0).GetComponent<Text> (),
-			transform.GetChild (1).GetComponent<Text> (),
-			transform.GetChild (2).GetComponent<Text> (),
-			transform.GetChild (3).GetComponent<Text> (),
-		};
+
 	}
 	
 
 	void Update () {
 		 
-		if (Input.GetKeyDown (KeyCode.UpArrow) && optionSlect < 1)
+		if (Input.GetKeyDown (KeyCode.UpArrow) && optionSlect >= 1)
 			optionSlect -= 1;
-		else if(Input.GetKeyDown(KeyCode.DownArrow) && optionSlect > maxOption)
+		else if(Input.GetKeyDown(KeyCode.DownArrow) && optionSlect < maxOption)
 			optionSlect += 1;
 
 
 
+		continueGame.color = notSelectColor;
+		newGame.color=notSelectColor;
+		optionsGame.color=notSelectColor;
+		exitGame.color=notSelectColor;
 
-		for (int i = 0; i < maxOption; i++) {
-			if (i != optionSlect)
-				textOptions[i].color = selectColor;
-			else
-				textOptions[i].color = notSelectColor;
+		switch (optionSlect) {
+		case 0:
+			continueGame.color = selectColor;
+			break;
+		case 1:
+			newGame.color=selectColor;
+			break;
+		case 2:
+			optionsGame.color=selectColor;
+			break;
+		case 3:
+			exitGame.color=selectColor;
+			break;
+		default:
+			continueGame.color = selectColor;
+			break;
 		}
 
 
