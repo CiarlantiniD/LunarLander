ef650bf6800e1cc1967b4fb279457b1e50389dd7
CiarlantiniD <dario.ciarlantini@outlook.com>
2017-06-27 21:06:16 -0300

    Se agrego aviso visual al tener poco conbustible

--- MoonLander/Assets/Scripts/UI Script/FuelUIScript.cs
+++ MoonLander/Assets/Scripts/UI Script/FuelUIScript.cs
@@ -6,14 +6,12 @@ public class FuelUIScript : MonoBehaviour {
 	UImanager parentScript;
 
 	Text fuelCount;
-    SpriteRenderer sr;
 
     int naveFuel;
     float timer = 0;
 
 	void Awake () {
         fuelCount = GetComponent<Text>();
-        sr = GetComponent<SpriteRenderer>();
 		parentScript = transform.GetComponentInParent<UImanager> ();
 	}
 		
@@ -21,24 +19,27 @@ public class FuelUIScript : MonoBehaviour {
         timer += Time.deltaTime;
         naveFuel = parentScript.naveFuel;
 
-		if (naveFuel < 0)
-			fuelCount.text = "Fuel   Empty";
+		if (naveFuel < 0) {
+            fuelCount.text = "Fuel   Empty";
+            fuelCount.enabled = true;
+        }
+			
         else
         {
             fuelCount.text = "Fuel   " + naveFuel.ToString();
 
             if (naveFuel < 200 && naveFuel > 0){
-                if(timer > 1f && sr.enabled == true)
-                {
-                    sr.enabled = false;
+
+                if (timer > 0.2f && fuelCount.enabled == true){
+                    fuelCount.enabled = false;
                     timer = 0;
                 }
-                else if (timer > 1f && sr.enabled == false){
-                    sr.enabled = true;
+                else if (timer > 0.2f && fuelCount.enabled == false){
+                    fuelCount.enabled = true;
                     timer = 0;
-                }
-                
+                }   
             }
+            
         }
 			
 	}
